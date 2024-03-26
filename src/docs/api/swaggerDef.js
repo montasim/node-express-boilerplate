0e76096139ae31cf5c59f725a06a5b5a8b6a1391
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-03-26 15:50:03 +0600

    [STILL WORKING] prepare for production

--- src/docs/api/swaggerDef.js
+++ src/docs/api/swaggerDef.js
@@ -1,11 +1,10 @@
-import packageJson from '../../../package.json' assert { type: 'json' };
 import config from '../../config/config.js';
 
 const swaggerDef = {
     openapi: '3.0.0',
     info: {
         title: 'node-express-boilerplate API documentation',
-        version: packageJson.version, // Access the version property from the imported packageJson object
+        version: '1.0.0',
         license: {
             name: 'MIT',
             url: 'https://github.com/montasim/node-express-boilerplate/blob/master/LICENSE',
